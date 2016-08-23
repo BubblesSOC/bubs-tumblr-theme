9ff09243bf3ead25536af25ace78cbae7df622f8
Sidney Collins <bubblessoc@gmail.com>
2016-08-23 07:29:25 -0400

    fixed an intermittent photoset height bug; started styling the posts

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Bubs' Tumblr Theme
 ==================
 
 It's a theme. For Tumblr. Crafted by Bubs.
+
+Credits
+-------
+
+- [imagesLoaded](http://imagesloaded.desandro.com/)
+- [Masonry](http://masonry.desandro.com/)
+- [infinite-scroll](https://github.com/infinite-scroll/infinite-scroll)
+- [NProgress.js](http://ricostacruz.com/nprogress/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -37,38 +37,105 @@
         // wrap the photos in row divs according to the layout array
         // ex: layout = ["2","2"]
         $.each(layout, function(i, columns) {
-            $photoset.children('.photo-link').slice(0, columns)
-                     .wrapAll('<div class="photoset-row columns-' + columns + '" />');
+            var $cols = $photoset.children('.photo-link').slice(0, columns);
+
+            $cols.wrapAll('<div class="photoset-row columns-' + columns + '" />')
+                 .css({
+                    'display': 'block',
+                    'padding-left': '5px',
+                    'float': 'left',
+                    'overflow': 'hidden',
+                    '-webkit-box-sizing': 'border-box',
+                    '-moz-box-sizing': 'border-box',
+                    '-o-box-sizing': 'border-box',
+                    'box-sizing': 'border-box'
+                 }).filter(':first-child').css({
+                    'padding-left': '0'
+                 });
+
+            if ( columns == '1' ) {
+                $cols.css('width', '100%');
+            } else if ( columns == '2' ) {
+                $cols.css('width', '50%');
+            } else if ( columns == '3' ) {
+                $cols.css('width', '33.3333333333%');
+            } else if ( columns == '4' ) {
+                $cols.css('width', '25%');
+            } else if ( columns == '5' ) {
+                $cols.css('width', '20%');
+            }
         });
 
-        var resizePhotoset = function() {
-            $photoset.children('.photoset-row').each(function() {
+        $photoset.children('.photoset-row').css({
+            'overflow': 'hidden',
+            'clear': 'left',
+            'margin-top': '5px',
+            'max-width': '1280px',
+        }).each(function( rowIdx ) {
+            var $row = $(this);
 
-                // equalize the heights of the row's images by setting the height
-                // of the row div to the height of the smallest image
-                // see: 'Photoset Grid' section in main.css
-
-                var $row  = $(this),
-                    $imgs = $row.find('img'),
-                    minHeight = Math.min.apply( null, $imgs.map(function() {
-                        return $(this).height();
-                    }).get() );
+            // equalize the heights of the row's images by setting the height
+            // of the row div to the height of the smallest image
+            var resizePhotosetRow = function( rowImages ) {
+                var minHeight = Math.min.apply(null, $.map(rowImages, function(image) {
+                    return image.img.height;
+                }));
 
                 $row.height(minHeight);
-                $imgs.each(function() {
-                    if ( $(this).height() == minHeight ) return true;
+                $.each(rowImages, function(i, image) {
+                    if ( image.img.height == minHeight ) return true;
 
                     // vertically center the larger images within view
-                    var top = 0 - Math.ceil( ($(this).height() - minHeight) / 2 );
-                    $(this).css('top', top + 'px');
+                    var top = 0 - Math.ceil( (image.img.height - minHeight) / 2 );
+                    $(image.img).css({
+                        'position': 'relative',
+                        'top': top + 'px'
+                    });
                 });
+            };
+
+            if ( rowIdx == 0 ) $row.css('margin-top', '0');
+
+            $row.imagesLoaded().done(function( instance ) {
+                // all images successfully loaded
+                resizePhotosetRow( instance.images );
+                $(window).on('resize', function() {
+                    resizePhotosetRow( instance.images );
+                });
+            }).fail(function() {
+                console.log('all images loaded, at least one is broken');
+            });
+        });
+    };
+
+
+    // remove '(via username)' from quote source
+    var removeQuoteVia = function( $post ) {
+        if ( !$post.hasClass('type-quote') || !$('body').hasClass('index') ) return;
+
+        var $src = $post.find('.quote-source'),
+            html = $src.html().replace(/&nbsp;/g, ' ');
+
+        html = html.replace(/(\s*)\(via\s+[^\)]+\)/gi, "$1");
+        $src.html(html);
+    };
+
+
+    // stretch tumblr gifs beyond the margins of .post-body and .caption
+    // see: '.tmblr-full.stretched' in main.css
+    var fixTumblrGifs = function( $post ) {
+        var $gifs = $post.find('.post-body figure.tmblr-full, .caption figure.tmblr-full');
+        if ( $gifs.length < 1 ) return;
+
+        var fixer = function() {
+            var width = $post.children('.post-content').width();
+            $gifs.addClass('stretched').width(width).each(function() {
+                $(this).find('.tmblr-attribution a').addClass('icon-link-ext');
             });
         };
 
-        $photoset.imagesLoaded(function() {
-            resizePhotoset();
-            $(window).on('resize', resizePhotoset);
-        });
+        fixer();
+        $(window).on('resize', fixer);
     };
 
 
@@ -80,6 +147,8 @@
 
             initLightbox( $post );
             initPhotosetGrid( $post );
+            removeQuoteVia( $post );
+            fixTumblrGifs( $post );
 
             // mark element with class to prevent duplicate work
             $post.addClass('idealized');
